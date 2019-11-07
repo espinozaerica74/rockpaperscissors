d62125fe7b1781c517c61d78970695d93b9003cd
espinozaerica74 <47925270+espinozaerica74@users.noreply.github.com>
2019-11-06 18:26:26 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,23 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+var userChoice = ""
+var computerChoice = ""
+var winner= ""
+var computerChoice = ""
 $("#shoot").click(function() {
 var userChoice = $("input").val();
 $("#userChoice").append(userChoice);
 
+ computerChoice = Math.random();
+console.log(computerChoice);
+if (computerChoice > .80){
+  $("#computerChoice").html("rock");
+}
+else if (computerchoice > .60){
+  $("#computerChoice").html("paper");
+}
+else{
+  $("#computerChoice").html("scissors");
+}
 });
